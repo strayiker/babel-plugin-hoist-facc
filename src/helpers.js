531b1ec775fc183b10e370548881609853881468
Kirill Menyankin <sadbox.games@gmail.com>
2018-10-02 23:31:50 +0300

    Fix the warning method closure

--- src/helpers.js
+++ src/helpers.js
@@ -2,7 +2,7 @@ import template from '@babel/template';
 
 export const buildWarning = template`
   function ID(fac) {
-    ID = function () { return fac };
+    ID = function (_fac) { return _fac };
     console.error('Warning: Failed to hoist a child function. This can be due to the fact that it refers on an identifiers of the higher scope, what blocks the hoisting.');
     return fac;
   }
